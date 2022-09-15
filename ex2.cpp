c24a9d03ffa4f1d9df3e76c535898aafc0eef699
xuan931002 <113594858+xuan931002@users.noreply.github.com>
2022-09-15 18:24:23 +0800

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World \n");
+    
+    cout <<  "hello erez \n";
+    cout <<  "my height is " << 1 << "m " << 80 << "cm";
+
+    return 0;
+}
