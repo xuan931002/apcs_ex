40af39638560d3c0970efe8945921b855e63f8d8
xuan931002 <113594858+xuan931002@users.noreply.github.com>
2022-09-15 18:47:38 +0800

    Create ex4.cpp

---
+++ ex4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout<< "          電腦不難販賣十月份銷售統計" << endl
+        << "產品          原價          數量          金額" << endl
+        << "電腦          $12,800       23            $294,400" << endl
+        << "160GB隨身碟   $7,800        19            $148,200" << endl
+        << "DVD Player    $11,000       132           $1,452,000" << endl
+        << "合計：                                    $1,894,600" << endl << endl << endl;
+    system("pause");
+    return 0;
+}
