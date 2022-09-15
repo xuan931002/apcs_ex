786689b624e3643cee37f51ece92d1d6cb15fbe1
xuan931002 <113594858+xuan931002@users.noreply.github.com>
2022-09-15 18:47:03 +0800

    Create ex3.cpp

---
+++ ex3.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (void) {
+     printf(" hello Selena \n"); 
+     printf(" my height is %d cm \n",180);
+     printf(" my height is %d m %2d cm \n",1,80);
+     printf(" my height is %d m %3d cm \n",1,80);
+     printf(" my height is %d m %4d cm \n",1,80);
+}
